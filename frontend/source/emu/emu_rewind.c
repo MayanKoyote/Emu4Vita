dc3f180463f869911a4f8c48e0a87994558fc05f
noword <noword.forever@gmail.com>
2024-02-22 22:23:03 +0800

    修正回溯代码

--- frontend/source/emu/emu_rewind.c
+++ frontend/source/emu/emu_rewind.c
@@ -20,18 +20,20 @@ enum BlockType
     BLOCK_DIFF
 };
 
+typedef struct BlockHeader BlockHeader;
 typedef struct FullBlock FullBlock;
+typedef struct DiffBlock DiffBlock;
 
-#define BLOCK_HEADER                                          \
-    int type;              /* BLOCK_FULL 或 BLOCK_DIFF*/     \
-    size_t total_size;     /* 整个 block 完整的大小 */ \
-    void *prev;            /* 指向上一个 block */        \
+#define BLOCK_HEADER                                      \
+    int type;              /* BLOCK_FULL 或 BLOCK_DIFF*/ \
+    BlockHeader *next;     /* 指向下一个 block */    \
+    BlockHeader *prev;     /* 指向上一个 block */    \
     FullBlock *full_block; /* type == BLOCK_FULL 时指向下一个 FullBlock, BLOCK_DIFF 时指向基础的 FullBlock */
 
-typedef struct
+struct BlockHeader
 {
     BLOCK_HEADER
-} BlockHeader;
+};
 
 // 用于保存完整的状态信息
 struct FullBlock
@@ -47,68 +49,74 @@ typedef struct
     uint32_t size;
 } DiffArea;
 
-typedef struct
+struct DiffBlock
 {
     BLOCK_HEADER
     uint32_t num;
     DiffArea areas[];
     // uin8_t *diff_buf;
-} DiffBlock;
+};
 
 // 缓存的全局状态
 typedef struct
 {
-    void *data;            // 分配得到的内存地址
-    void *current;         // 指向当前的状态
+    uint8_t *data;         // 分配得到的内存地址
+    BlockHeader *current;  // 指向当前的状态
     FullBlock *header;     // 指向最早的状态
-    void *tail;            // 指向 data 的尾部
+    uint8_t *tail;         // 指向 data 的尾部
     size_t state_size;     // 完整状态的实际大小
     size_t threshold_size; // state_size * THRESHOLD_RATE
+    size_t buf_size;       // 缓存大小，用于判断是否新开一个完全状态
     uint64_t next_time;    // 下一次记录的时间
-    void *tmp_state;       // 用于保存差异状态
+    uint8_t *tmp_state;    // 用于保存差异状态
 } RewindState;
 
 RewindState rs = {0};
 int rewind_key_pressed = 0;
 int in_rewinding = 0;
 
-static void *SetNextBlock(BlockHeader *bh)
+static void *GetNextBlock(BlockHeader *bh)
 {
-    void *next = (uint8_t *)bh + bh->total_size;
+    uint8_t *next = (uint8_t *)(bh->next);
     if (next > rs.tail - rs.state_size - 0x20)
         next = rs.data;
 
-    void *start1 = next;
-    void *end1 = (uint8_t *)next + sizeof(FullBlock) + rs.state_size;
-    void *start2 = rs.header;
-    void *end2 = (uint8_t *)rs.header + rs.header->total_size;
-
-    if (!(end1 < start2 || start1 >= end2))
+    if (next < (uint8_t *)rs.header->next && next + sizeof(FullBlock) + rs.state_size > (uint8_t *)rs.header)
     {
-        // next 落入 header 的区域
+        // next 块落入 header 的区域
+        AppLog("SetNextBlock %08x %08x %08x\n", next, rs.header, rs.header->full_block);
         if (rs.header->full_block)
-            rs.header = rs.header->full_block; // 指向下一个 FullBlock
+            rs.header = rs.header->full_block; // rs.header 指向下一个 FullBlock
         else
             next = NULL; // 只有一个 FullBlock
     }
     return next;
 }
 
-static void SaveFullState(FullBlock *fullb)
+static void SaveFullState(FullBlock *fullb, BlockHeader *prev)
 {
     fullb->type = BLOCK_FULL;
-    fullb->total_size = sizeof(FullBlock) + rs.state_size;
-    fullb->prev = 0;
+    fullb->next = (BlockHeader *)(((uint8_t *)fullb) + sizeof(FullBlock) + rs.state_size);
+    fullb->prev = NULL;
     fullb->full_block = NULL;
     retro_serialize(fullb->buf, rs.state_size);
+    AppLog("SaveFullState %08x %08x\n", fullb, fullb->next);
+
+    if (prev)
+    {
+        if (prev->type == BLOCK_FULL)
+            prev->full_block = fullb;
+        else
+            prev->full_block->full_block = fullb;
+    }
 }
 
 // 仅把需要复制的差异信息(地址, 大小)写入 areas，统计 total_size
 static void PreSaveDiffState(DiffBlock *diffb, FullBlock *fullb)
 {
     diffb->type = BLOCK_DIFF;
-    diffb->total_size = sizeof(DiffBlock);
-    diffb->prev = 0;
+    size_t total_size = sizeof(DiffBlock);
+    diffb->prev = NULL;
     diffb->full_block = fullb;
     diffb->num = 0;
     retro_serialize(rs.tmp_state, rs.state_size);
@@ -125,7 +133,7 @@ static void PreSaveDiffState(DiffBlock *diffb, FullBlock *fullb)
             {
                 last_state = 0;
                 diffb->areas[diffb->num].size = offset - diffb->areas[diffb->num].offset;
-                diffb->total_size += diffb->areas[diffb->num].size;
+                total_size += diffb->areas[diffb->num].size;
                 diffb->num++;
             }
         }
@@ -153,17 +161,19 @@ static void PreSaveDiffState(DiffBlock *diffb, FullBlock *fullb)
             diffb->areas[diffb->num].size = rs.state_size - diffb->areas[diffb->num].offset;
         }
 
-        diffb->total_size += diffb->areas[diffb->num].size;
+        total_size += diffb->areas[diffb->num].size;
         diffb->num++;
     }
     else if (last_state == 1)
     {
         diffb->areas[diffb->num].size = offset - diffb->areas[diffb->num].offset;
-        diffb->total_size += diffb->areas[diffb->num].size;
+        total_size += diffb->areas[diffb->num].size;
         diffb->num++;
     }
 
-    diffb->total_size += diffb->num * sizeof(DiffArea);
+    total_size += diffb->num * sizeof(DiffArea);
+
+    diffb->next = (BlockHeader *)((uint8_t *)diffb + total_size);
 }
 
 static void SaveDiffState(DiffBlock *diffb)
@@ -206,6 +216,7 @@ void Emu_InitRewind(size_t buffer_size)
 
     rs.state_size = retro_serialize_size();
     rs.threshold_size = rs.state_size * THRESHOLD_RATE;
+    rs.buf_size = buffer_size;
     rs.data = malloc(buffer_size);
     rs.header = NULL;
     rs.current = NULL;
@@ -227,7 +238,7 @@ void Emu_DeinitRewind()
 
 static void Rewind()
 {
-    if (rs.current && rs.current != rs.header)
+    if (rs.current && rs.current != (BlockHeader *)rs.header)
     {
         if (Emu_GetCurrentRunSpeed() > 0.1)
         {
@@ -237,44 +248,55 @@ static void Rewind()
         }
         const void *state = GetState();
         retro_unserialize(state, rs.state_size);
-        rs.current = ((BlockHeader *)rs.current)->prev;
+        rs.current = rs.current->prev;
         Emu_CleanAudioSound();
     }
 
     rewind_key_pressed = 0;
 }
 
+static size_t GetDistance()
+{
+    if ((size_t)rs.current >= (size_t)rs.header)
+        return (size_t)rs.current - (size_t)rs.header;
+    else
+        return rs.buf_size + (size_t)rs.header - (size_t)rs.current;
+}
+
 static void SaveState()
 {
     if (rs.current == NULL)
     {
-        rs.current = rs.data;
-        rs.header = rs.data;
-        SaveFullState((FullBlock *)rs.current);
+    ALL_NEW_STATE:
+        rs.current = (BlockHeader *)rs.data;
+        rs.header = (FullBlock *)rs.data;
+        SaveFullState(rs.header, NULL);
     }
     else
     {
-        BlockHeader *current = (BlockHeader *)rs.current;
-        BlockHeader *next = SetNextBlock(current);
+        BlockHeader *current = rs.current;
+        BlockHeader *next = GetNextBlock(current);
+
         if (next)
         {
-            FullBlock *fb = current->type == BLOCK_FULL ? (FullBlock *)current : ((DiffBlock *)current)->full_block;
-            PreSaveDiffState((DiffBlock *)next, fb);
-            if (next->total_size > rs.threshold_size)
-            {
-                SaveFullState((FullBlock *)next);
-                if (current->type == BLOCK_FULL)
-                    current->full_block = (FullBlock *)next;
-                else
-                    current->full_block->full_block = (FullBlock *)next;
-            }
+            if ((!rs.header->full_block) && GetDistance() > rs.buf_size / 2)
+                SaveFullState((FullBlock *)next, current);
             else
             {
-                SaveDiffState((DiffBlock *)next);
+                FullBlock *fb = current->type == BLOCK_FULL ? (FullBlock *)current : ((DiffBlock *)current)->full_block;
+                PreSaveDiffState((DiffBlock *)next, fb);
+                if ((size_t)(next->next) - (size_t)next > rs.threshold_size)
+                    SaveFullState((FullBlock *)next, current);
+                else
+                    SaveDiffState((DiffBlock *)next);
             }
 
             rs.current = next;
             next->prev = current;
+        }
+        else
+        {
+            goto ALL_NEW_STATE;
         }
     }
 
